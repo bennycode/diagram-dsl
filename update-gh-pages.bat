4376514c6136adddc1817c6305927235305db14d
Benny Neugebauer <bn@bennyn.de>
2014-10-31 15:37:35 +0100

    Turned batch file into one-line

--- update-gh-pages.bat
+++ update-gh-pages.bat
@@ -1,6 +1,1 @@
-git checkout master
-git push origin master
-git checkout gh-pages
-git merge master
-git push origin gh-pages
-git checkout master
+git checkout master && git push origin master && git checkout gh-pages && git merge master && git push origin gh-pages && git checkout master
