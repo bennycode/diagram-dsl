59e03aa0fcbc6cecbd35c2624ea24ad468d71c4e
Benny Neugebauer <bn@bennyn.de>
2014-11-21 14:20:42 +0100

    Added missing semicolon in livereload script.

--- demo/demo.js
+++ demo/demo.js
@@ -3,5 +3,5 @@ if (window.location.hostname === 'localhost') {
   livereload.id = 'livereload';
   livereload.src = '//localhost:36963/livereload.js';
   document.body.appendChild(livereload);
-  console.info('Live Reload enabled: https://github.com/gruntjs/grunt-contrib-watch/blob/master/docs/watch-examples.md')
+  console.info('Live Reload enabled: https://github.com/gruntjs/grunt-contrib-watch/blob/master/docs/watch-examples.md');
 }
